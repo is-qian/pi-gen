a690a3feef1b1614c3dd32b31c1ff10849dde8ea
is-qian <tangruiqian17@gmail.com>
2024-04-23 19:40:13 +0800

    test1

--- stage4a/00-reTerminal/00-run.sh
+++ stage4a/00-reTerminal/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 set -x
-
+GIT_MODULE = 'http://192.168.1.77:1080/awesome-se/seeed-linux-dtoverlays.git -b master --depth=1'
+SEEED_DEV_NAME = SEEED_DEV_NAME=reComputer-R100x
 
 if [ "X$GIT_MODULE" != "X" ]; then
 	MODULE_PATH=/tmp/seeed-linux-dtoverlays
