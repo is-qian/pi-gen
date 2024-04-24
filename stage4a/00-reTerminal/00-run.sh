6d2c62a229d6a0f378d3256a11b38c17db52bd8d
is-qian <tangruiqian17@gmail.com>
2024-04-24 14:27:29 +0800

    testb

--- stage4a/00-reTerminal/00-run.sh
+++ stage4a/00-reTerminal/00-run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash -e
 set -x
 
-GIT_MODULE='http://192.168.1.77:1080/awesome-se/seeed-linux-dtoverlays.git -b master --depth=1'
+GIT_MODULE='https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git -b master --depth=1'
 SEEED_DEV_NAME=SEEED_DEV_NAME=reComputer-R100x
 
 if [ "X$GIT_MODULE" != "X" ]; then
 	MODULE_PATH=/tmp/seeed-linux-dtoverlays
 	${PROXYCHAINS} git clone ${GIT_MODULE} "${ROOTFS_DIR}${MODULE_PATH}"
-	${PROXYCHAINS} wget http://192.168.1.77/reTerminalDM/dt-blob-disp1-cam2.bin -O "${ROOTFS_DIR}/boot/dt-blob.bin"
-
 	on_chroot << EOF
 cd ${MODULE_PATH}
 dpkg -l | grep kernel
