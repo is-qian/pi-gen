366289eaa5ddec966a98dae2547768e26eab51c7
is-qian <tangruiqian17@gmail.com>
2024-04-27 06:01:29 +0800

    test22

--- stage4a/package-test/00-run.sh
+++
@@ -1,8 +0,0 @@
-          #!/bin/bash
-          MODULE_PATH=/tmp/seeed-linux-dtoverlays
-          apt-get update
-          apt-get -y install dialog coreutils quilt parted qemu-user-static debootstrap zerofree zip dosfstools libarchive-tools libcap2-bin grep rsync xz-utils file git curl bc qemu-utils kpartx gpg pigz xxd kmod binfmt-support
-          git clone https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git -b master --depth=1
-          cd seeed-linux-dtoverlays
-          ls -l
-          ./scripts/reTerminal.sh --device reComputer-R100x
