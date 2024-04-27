debf0dc77e9149b3e66c5ab983e04f6db83218bd
is-qian <tangruiqian17@gmail.com>
2024-04-27 12:16:06 +0800

    test26

--- stage4a/package-test/00-run-chroot.sh
+++ stage4a/package-test/00-run-chroot.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 MODULE_PATH=/tmp/seeed-linux-dtoverlays
 apt-get update
-rpi-update
 apt-get -y install dialog coreutils quilt parted qemu-user-static debootstrap zerofree zip dosfstools libarchive-tools libcap2-bin grep rsync xz-utils file git curl bc qemu-utils kpartx gpg pigz xxd kmod binfmt-support
 git clone https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git -b master --depth=1
 cd seeed-linux-dtoverlays
