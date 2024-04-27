1ad61fe763a5d31355e3df23f32a9e8feb284b43
is-qian <tangruiqian17@gmail.com>
2024-04-28 00:18:33 +0800

    test2d

--- stage4a/package-test/00-run-chroot.sh
+++ stage4a/package-test/00-run-chroot.sh
@@ -2,7 +2,7 @@
 MODULE_PATH=/tmp/seeed-linux-dtoverlays
 apt-get update
 apt-get -y install dialog coreutils quilt parted qemu-user-static debootstrap zerofree zip dosfstools libarchive-tools libcap2-bin grep rsync xz-utils file git curl bc qemu-utils kpartx gpg pigz xxd kmod binfmt-support
-git clone https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git -b master --depth=1 -O "${ROOTFS_DIR}${MODULE_PATH}"
+git clone https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git "${ROOTFS_DIR}${MODULE_PATH}"
 ls -l
 pwd
 find / -name "0001-compatible-for-pi-gen.patch"
