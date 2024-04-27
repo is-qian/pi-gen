a5598b8ed3718efd90bd085bdafdb10c63917a0a
is-qian <tangruiqian17@gmail.com>
2024-04-27 23:55:30 +0800

    test2c

--- stage4a/package-test/00-run-chroot.sh
+++ stage4a/package-test/00-run-chroot.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 MODULE_PATH=/tmp/seeed-linux-dtoverlays
 apt-get update
 apt-get -y install dialog coreutils quilt parted qemu-user-static debootstrap zerofree zip dosfstools libarchive-tools libcap2-bin grep rsync xz-utils file git curl bc qemu-utils kpartx gpg pigz xxd kmod binfmt-support
-git clone https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git -b master --depth=1
+git clone https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git -b master --depth=1 -O "${ROOTFS_DIR}${MODULE_PATH}"
 ls -l
 pwd
-cp 0001-compatible-for-pi-gen.patch seeed-linux-dtoverlays/
-cd seeed-linux-dtoverlays
+find / -name "0001-compatible-for-pi-gen.patch"
+cp /home/runner/work/pi-gen/pi-gen/stage4a/package-test/0001-compatible-for-pi-gen.patch "${ROOTFS_DIR}${MODULE_PATH}"
+cd ${MODULE_PATH}
 ls -l
 git apply 0001-compatible-for-pi-gen.patch
 ./scripts/reTerminal.sh --device reComputer-R100x --keep-kernel 
