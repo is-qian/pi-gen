46e44baea1230eb1ea3f5f210af3fbcc858afa0d
is-qian <tangruiqian17@gmail.com>
2024-04-28 12:11:57 +0800

    test3b

--- test-stage/package-test/00-run-chroot.sh
+++ test-stage/package-test/00-run-chroot.sh
@@ -3,11 +3,11 @@ set -x
 MODULE_PATH=/tmp/seeed-linux-dtoverlays
 apt-get update
 apt-get -y install dialog coreutils quilt parted qemu-user-static debootstrap zerofree zip dosfstools libarchive-tools libcap2-bin grep rsync xz-utils file git curl bc qemu-utils kpartx gpg pigz xxd kmod binfmt-support
-git clone https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git "${ROOTFS_DIR}${MODULE_PATH}"
+git clone https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git 
 ls -l
 pwd
 uname -a
-cd ${ROOTFS_DIR}${MODULE_PATH}
+cd seeed-linux-dtoverlays
 ls -l
 uname -a
 ./scripts/reTerminal.sh --device reComputer-R100x --keep-kernel 
