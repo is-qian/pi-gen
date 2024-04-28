f3978ea16fd17340e1d498d7011844be6431d585
is-qian <tangruiqian17@gmail.com>
2024-04-28 11:05:08 +0800

    test38

--- test-stage/package-test/00-run-chroot.sh
+++ test-stage/package-test/00-run-chroot.sh
@@ -6,11 +6,8 @@ apt-get -y install dialog coreutils quilt parted qemu-user-static debootstrap ze
 git clone https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git "${ROOTFS_DIR}${MODULE_PATH}"
 ls -l
 pwd
-find / -name "0001-compatible-for-pi-gen.patch"
-cp /home/runner/work/pi-gen/pi-gen/stage4a/package-test/0001-compatible-for-pi-gen.patch "${ROOTFS_DIR}${MODULE_PATH}"
 uname -a
 cd ${ROOTFS_DIR}${MODULE_PATH}
 ls -l
 uname -a
-git apply 0001-compatible-for-pi-gen.patch
 ./scripts/reTerminal.sh --device reComputer-R100x --keep-kernel 
