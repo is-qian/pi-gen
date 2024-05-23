3c82ca93376e288aaa716a34280e007cc7b41025
is-qian <tangruiqian17@gmail.com>
2024-05-23 18:07:41 +0800

    test102

--- generateReadme.py
+++ generateReadme.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+def addIntro(f):
+	txt = '''  
 # pi-gen-expand
 
 此工程用于自动生成适配多个设备的树莓派系统。可以在workflow中指定设备和配置，然后自动生成对应的树莓派系统。
@@ -7,3 +11,14 @@
 - reTerminal: https://github.com/ddvk/remarkable2-framebuffer
 - reTerminal-plus: https://github.com/ddvk/remarkable2-framebuffer
 - reComputer-R100X: https://github.com/ddvk/remarkable2-framebuffer
+
+''' 
+	f.write(txt)
+
+
+
+
+if __name__=='__main__':
+	f = open('README.md', 'w+')
+	addIntro(f)
+	f.close 
